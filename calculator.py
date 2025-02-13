2cf1b23a5434ac99eb620b5ef8741c58978b44f9
Efraileg <94174098+Efraileg@users.noreply.github.com>
2025-02-13 02:41:02 +0100

    Mejoras en calculator.py
    Se han añadido las mejoras indicadas en la issue implementando una función "main" y "match-case" para aumentar la legibilidad del código.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+
 # Custom function to read the two numbers.
 def read_numbers():
     num1 = float(input('Enter first number: '))
@@ -13,36 +14,43 @@ print('    3. Multiplication')
 print('    4. Division')
 print('    5. Exit')
 
-# Main loop.
-while True:
+def main():
+    
+    # Main loop.
+    while True:
+        
+        choice = input('\nEnter your choice (1-5): ')
 
-    choice = input('\nEnter your choice (1-5): ')
-
-    if choice in ('1', '2', '3', '4', '5'):
-        if choice == '1':
-            num1, num2 = read_numbers()
-            result = num1 + num2
-            print(f'Result: {result}')
-        
-        elif choice == '2':
-            num1, num2 = read_numbers()
-            result = num1 - num2
-            print(f'Result: {result}')
-        
-        elif choice == '3':
-            num1, num2 = read_numbers()
-            result = num1 * num2
-            print(f'Result: {result}')
-        
-        elif choice == '4':
-            try:
+        match choice:
+            case '1':
                 num1, num2 = read_numbers()
-                result = num1 / num2
+                result = num1 + num2
                 print(f'Result: {result}')
-            except ZeroDivisionError:
-                print("Error: division by zero")
-        
-        elif choice == '5':
-            break
-    else:
-        print('Invalid Input')
+            
+            case '2':
+                num1, num2 = read_numbers()
+                result = num1 - num2
+                print(f'Result: {result}')
+            
+            case '3':
+                num1, num2 = read_numbers()
+                result = num1 * num2
+                print(f'Result: {result}')
+            
+            case '4':
+                try:
+                    num1, num2 = read_numbers()
+                    result = num1 / num2
+                    print(f'Result: {result}')
+                
+                except ZeroDivisionError:
+                    print("Error: division by zero")
+            
+            case '5':
+                break
+            
+            case _:
+                print('Invalid Input')
+
+if __name__ == "__main__":
+    main()
